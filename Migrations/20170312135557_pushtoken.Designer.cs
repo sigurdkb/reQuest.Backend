ea5ebbc811a9484c034e7a6c3a0f0f606fbf9630
Sigurd Brinch <sigurd.k.brinch@uia.no>
2017-03-12 14:56:15 +0100

    push token

--- Migrations/20170312135557_pushtoken.Designer.cs
+++ Migrations/20170312135557_pushtoken.Designer.cs
@@ -8,9 +8,10 @@ using reQuest.Backend.Entities;
 namespace reQuest.Backend.Migrations
 {
     [DbContext(typeof(reQuestDbContext))]
-    partial class reQuestDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170312135557_pushtoken")]
+    partial class pushtoken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
